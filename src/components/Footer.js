79dedef179d4ff98c77c1959582ec33432c5c110
sukant43 <sukant98657@gmail.com>
2025-01-07 19:54:33 +0530

    Re-add project without submodule

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+                    <a
+                        href="https://github.com/gulshancodes/react-shopping-cart"
+                        className="repo_link"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        
+                    </a>
+
+                  
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
